b279dce1442cf0f6aa1116d6626a403b08fd277d
Hurricaaane <git@ha3.eu>
2012-04-13 13:22:02 +0200

    Made Ha3ModCore abstract
    Added methods for chat packets and custom packets (plugin channels)
    Updated ModLoader impl.

--- mc-src/eu/ha3/mc/mod/Ha3ModCore.java
+++ mc-src/eu/ha3/mc/mod/Ha3ModCore.java
@@ -1,19 +1,60 @@
 package eu.ha3.mc.mod;
 
 import net.minecraft.src.KeyBinding;
+import net.minecraft.src.Packet250CustomPayload;
 
-public interface Ha3ModCore
+public abstract class Ha3ModCore
 {
-	public void setMod(Ha3Mod modIn);
+	public abstract void setMod(Ha3Mod modIn);
 	
-	public void doFrame(float fspan);
-	
-	public void doKeyBindingEvent(KeyBinding event);
-	
-	public void load();
+	public abstract void load();
 	
 	//public void unload();
 	
-	public void doManagerReady();
+	/**
+	 * If uses frame, triggers when a frame event occurs.
+	 * 
+	 * @param fspan
+	 */
+	public void doFrame(float fspan)
+	{
+	};
+	
+	/**
+	 * When a KeyBinding event occurs.
+	 * 
+	 * @param fspan
+	 */
+	public void doKeyBindingEvent(KeyBinding event)
+	{
+	};
+	
+	/**
+	 * Trigger when the Mod Manager turns into a "ready" state (i.e. when all
+	 * mods are loaded).
+	 * 
+	 * @param fspan
+	 */
+	public void doManagerReady()
+	{
+	};
+	
+	/**
+	 * Triggers when a packet subscribed to arrives.
+	 * 
+	 * @param fspan
+	 */
+	public void doPluginChannelPacket(Packet250CustomPayload packet)
+	{
+	};
+	
+	/**
+	 * If uses chat, triggers when a chat occurs.
+	 * 
+	 * @param fspan
+	 */
+	public void doChat(String contents)
+	{
+	};
 	
 }
