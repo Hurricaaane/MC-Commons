93e4a75ee8a4e4d54dc56769a577d2a4c27e841e
Hurricaaane <git@ha3.eu>
2012-04-13 13:34:28 +0200

    Made Ha3ModCore abstract
    Added methods for chat packets and custom packets (plugin channels)
    Updated ModLoader impl.

--- mc-src/eu/ha3/mc/mod/Ha3ModManager.java
+++ mc-src/eu/ha3/mc/mod/Ha3ModManager.java
@@ -2,6 +2,7 @@ package eu.ha3.mc.mod;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.src.KeyBinding;
+import net.minecraft.src.Packet250CustomPayload;
 
 public interface Ha3ModManager
 {
@@ -11,11 +12,17 @@ public interface Ha3ModManager
 	
 	public void setUsesFrame(boolean enable);
 	
+	public void setReceivesChat(boolean enable);
+	
+	public void subscribeToPluginChannel(String channel);
+	
 	//public void setUsesKeyBindingEvent(boolean enable);
 	
 	public void addKeyBinding(KeyBinding keyBindingIn, String localization);
 	
 	public boolean getUsesFrame();
+	
+	public boolean getReceivesChat();
 	
 	//public boolean getUsesKeyBindingEvent();
 	
@@ -23,16 +30,20 @@ public interface Ha3ModManager
 	
 	public void communicateKeyBindingEvent(KeyBinding event);
 	
+	public void communicateChat(String contents);
+	
+	public void communicatePluginChannelPacket(Packet250CustomPayload packet);
+	
 	public void communicateManagerReady();
 	
 	@SuppressWarnings("rawtypes")
 	public Object getPrivateValue(Class classToPerformOn,
 			Object instanceToPerformOn, int zeroOffsets)
-	throws Ha3ModPrivateAccessException;
+					throws Ha3ModPrivateAccessException;
 	
 	@SuppressWarnings("rawtypes")
 	public void setPrivateValue(Class classToPerformOn,
 			Object instanceToPerformOn, int zeroOffsets, Object newValue)
-	throws Ha3ModPrivateAccessException;
+					throws Ha3ModPrivateAccessException;
 	
 }
