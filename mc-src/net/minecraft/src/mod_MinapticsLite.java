8bb2e930b1d7b78935bd74f882eb896b65e4b835
Hurricaaane <git@ha3.eu>
2012-06-21 13:32:21 +0200

    MinapticsLite: Now using Haddon

---
+++ mc-src/net/minecraft/src/mod_MinapticsLite.java
@@ -0,0 +1,18 @@
+package net.minecraft.src;
+
+
+public class mod_MinapticsLite extends HaddonBridgeModLoader
+{
+	public mod_MinapticsLite()
+	{
+		super(new MinapticsLite());
+		
+	}
+	
+	@Override
+	public String getVersion()
+	{
+		return "r14 for ...";
+	}
+	
+}
