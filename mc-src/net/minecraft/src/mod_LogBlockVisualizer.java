d533e0382c835154c82fa4fb2c5c573f735916c6
Hurricaaane <git@ha3.eu>
2012-06-24 02:24:40 +0200

    Renamed LogBlockVisualizer mod class

---
+++ mc-src/net/minecraft/src/mod_LogBlockVisualizer.java
@@ -0,0 +1,12 @@
+package net.minecraft.src;
+
+
+public class mod_LogBlockVisualizer extends HaddonBridgeModLoader
+{
+	public mod_LogBlockVisualizer()
+	{
+		super(new LBVisHaddon());
+		
+	}
+	
+}
