2a50ca1a026428a4a65241f6cec189c25fff9600
Hurricaaane <git@ha3.eu>
2012-06-27 04:32:50 +0200

    Added other units

---
+++ mc-src/eu/ha3/matmos3/units/model/MachineUnit.java
@@ -0,0 +1,13 @@
+package eu.ha3.matmos3.units.model;
+
+public interface MachineUnit
+{
+	public boolean isTrue();
+	
+	public void turnOn();
+	
+	public void turnOff();
+	
+	public void interrupt();
+
+}
