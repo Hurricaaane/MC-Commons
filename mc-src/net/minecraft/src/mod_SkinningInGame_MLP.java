ea9d4514f85c233db830bad8fc90afe30e0b6af1
Hurricaaane <git@ha3.eu>
2012-07-03 22:24:53 +0200

    whoops forfor to commit this to SkinningInGame (class names)

--- mc-src/net/minecraft/src/mod_SkinningInGame_MLP.java
+++ mc-src/net/minecraft/src/mod_SkinningInGame_MLP.java
@@ -3,10 +3,9 @@ package net.minecraft.src;
 
 public class mod_SkinningInGame_MLP extends HaddonBridgeModLoader
 {
-	
 	public mod_SkinningInGame_MLP()
 	{
-		super(new SkinningInGame());
+		super(new SkinningInGameHaddon_MLP());
 		
 	}
 	
