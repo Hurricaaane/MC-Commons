d13a9887c2a7dfca8f82df854e14ebd6b8218e42
Hurricaaane <git@ha3.eu>
2012-07-15 18:53:17 +0200

    Instead of loading expansions at once, load one per frame.

--- mc-src/net/minecraft/src/MAtExpansionLoader.java
+++ mc-src/net/minecraft/src/MAtExpansionLoader.java
@@ -17,20 +17,20 @@ import java.util.concurrent.ConcurrentHashMap;
 import net.minecraft.client.Minecraft;
 
 /*
-            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
-                    Version 2, December 2004 
+            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+                    Version 2, December 2004
 
- Copyright (C) 2004 Sam Hocevar <sam@hocevar.net> 
+ Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
 
- Everyone is permitted to copy and distribute verbatim or modified 
- copies of this license document, and changing it is allowed as long 
- as the name is changed. 
+ Everyone is permitted to copy and distribute verbatim or modified
+ copies of this license document, and changing it is allowed as long
+ as the name is changed.
 
-            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
-   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION 
+            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
 
-  0. You just DO WHAT THE FUCK YOU WANT TO. 
-*/
+  0. You just DO WHAT THE FUCK YOU WANT TO.
+ */
 
 public class MAtExpansionLoader
 {
@@ -71,7 +71,7 @@ public class MAtExpansionLoader
 			onlineStorageFolder.mkdirs();
 		
 		tasks = new ArrayList<Runnable>();
-
+		
 	}
 	
 	public synchronized void renewProngs()
@@ -341,22 +341,17 @@ public class MAtExpansionLoader
 	{
 		if (!tasks.isEmpty())
 		{
-			for (Runnable runnable : tasks)
+			try
 			{
-				try
-				{
-					runnable.run();
-					
-				}
-				catch (Exception e)
-				{
-				}
+				tasks.remove(0).run();
 				
 			}
-			tasks.clear();
+			catch (Exception e)
+			{
+			}
 			
 		}
-
+		
 		for (MAtExpansion expansion : expansions.values())
 		{
 			expansion.dataRoutine();
@@ -491,5 +486,5 @@ public class MAtExpansionLoader
 		tasks.add(runnable);
 		
 	}
-
+	
 }
