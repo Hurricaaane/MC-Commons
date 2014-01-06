698b238e50e72149f5a325f087310970544d04de
Hurricaaane <git@ha3.eu>
2014-01-06 18:03:51 +0100

    Made Chatter commons

---
+++ mc-src/eu/ha3/mc/quick/chat/ChatColorsSimple.java
@@ -0,0 +1,30 @@
+package eu.ha3.mc.quick.chat;
+
+/* x-placeholder-wtfplv2 */
+
+public class ChatColorsSimple
+{
+	public static final String COLOR_BLACK = "\u00A70";
+	public static final String COLOR_DARKBLUE = "\u00A71";
+	public static final String COLOR_DARKGREEN = "\u00A72";
+	public static final String COLOR_TEAL = "\u00A73";
+	public static final String COLOR_DARKRED = "\u00A74";
+	public static final String COLOR_PURPLE = "\u00A75";
+	public static final String COLOR_GOLD = "\u00A76";
+	public static final String COLOR_GRAY = "\u00A77";
+	public static final String COLOR_DARKGRAY = "\u00A78";
+	public static final String COLOR_BLUE = "\u00A79";
+	public static final String COLOR_BRIGHTGREEN = "\u00A7a";
+	public static final String COLOR_AQUA = "\u00A7b";
+	public static final String COLOR_RED = "\u00A7c";
+	public static final String COLOR_PINK = "\u00A7d";
+	public static final String COLOR_YELLOW = "\u00A7e";
+	public static final String COLOR_WHITE = "\u00A7f";
+	
+	public static final String THEN_SCRAMBLE = "\u00A7k";
+	public static final String THEN_BOLD = "\u00A7l";
+	public static final String THEN_STRIKETHROUGH = "\u00A7m";
+	public static final String THEN_UNDERLINE = "\u00A7n";
+	public static final String THEN_ITALIC = "\u00A7o";
+	public static final String THEN_RESET = "\u00A7r";
+}
---
+++ mc-src/eu/ha3/mc/quick/chat/HasChatter.java
@@ -0,0 +1,10 @@
+package eu.ha3.mc.quick.chat;
+
+/*
+--filenotes-placeholder
+*/
+
+public interface HasChatter
+{
+	public Chatter getChatter();
+}
