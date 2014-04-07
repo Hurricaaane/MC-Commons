1369d37a311552d5ebfa6bda49082f535fb89192
Andrew Gilmore <agilmore2@users.noreply.github.com>
2014-04-07 12:05:57 -0600

    Move BridgeModLoader to LiteBase

--- mc-src/eu/ha3/mc/haddon/litemod/LiteBase.java
+++ mc-src/eu/ha3/mc/haddon/litemod/LiteBase.java
@@ -1,5 +1,6 @@
 package eu.ha3.mc.haddon.litemod;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -17,8 +18,6 @@ import eu.ha3.mc.haddon.SupportsChatEvents;
 import eu.ha3.mc.haddon.SupportsConnectEvents;
 import eu.ha3.mc.haddon.SupportsEverythingReady;
 import eu.ha3.mc.haddon.SupportsFrameEvents;
-import eu.ha3.mc.haddon.SupportsGuiFrameEvents;
-import eu.ha3.mc.haddon.SupportsGuiTickEvents;
 import eu.ha3.mc.haddon.SupportsIncomingMessages;
 import eu.ha3.mc.haddon.SupportsInitialization;
 import eu.ha3.mc.haddon.SupportsKeyEvents;
@@ -27,31 +26,23 @@ import eu.ha3.mc.haddon.UnsupportedInterfaceException;
 import eu.ha3.mc.haddon.Utility;
 import eu.ha3.mc.haddon.implem.HaddonUtilityModLoader;
 
+import com.mumfrey.liteloader.InitCompleteListener;
+import com.mumfrey.liteloader.LiteMod;
+import com.mumfrey.liteloader.core.LiteLoader;
+
+import eu.ha3.mc.haddon.implem.HaddonUtilityImpl;
+
 /*
-            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
-                    Version 2, December 2004 
-
- Copyright (C) 2004 Sam Hocevar <sam@hocevar.net> 
-
- Everyone is permitted to copy and distribute verbatim or modified 
- copies of this license document, and changing it is allowed as long 
- as the name is changed. 
-
-            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
-   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION 
-
-  0. You just DO WHAT THE FUCK YOU WANT TO. 
+--filenotes-placeholder
 */
 
-public class HaddonBridgeModLoader extends BaseMod implements Manager, Bridge
+public class LiteBase implements LiteMod, Manager, Bridge
 {
 	private Haddon haddon;
 	private Utility utility;
 	
 	private boolean supportsTick;
 	private boolean supportsFrame;
-	private boolean supportsGuiTick;
-	private boolean supportsGuiFrame;
 	private boolean supportsChat;
 	private boolean supportsKey;
 	private boolean supportsIncomingMessages;
@@ -63,21 +54,17 @@ public class HaddonBridgeModLoader extends BaseMod implements Manager, Bridge
 	
 	private boolean tickEnabled;
 	private boolean frameEnabled;
-	private boolean guiTickEnabled;
-	private boolean guiFrameEnabled;
 	private boolean chatEnabled;
 	
 	private Map<Object, Object> renderPairs;
 	
 	private boolean impl_continueTicking;
-	private boolean impl_continueGuiTicking;
 	
 	private long nextTick;
-	private long lastGuiTick;
 	
 	private long ticksRan;
 	
-	public HaddonBridgeModLoader(Haddon haddon)
+	public LiteBase(Haddon haddon)
 	{
 		this.haddon = haddon;
 		
@@ -86,8 +73,6 @@ public class HaddonBridgeModLoader extends BaseMod implements Manager, Bridge
 		
 		this.supportsTick = haddon instanceof SupportsTickEvents;
 		this.supportsFrame = haddon instanceof SupportsFrameEvents;
-		this.supportsGuiTick = haddon instanceof SupportsGuiTickEvents;
-		this.supportsGuiFrame = haddon instanceof SupportsGuiFrameEvents;
 		this.supportsChat = haddon instanceof SupportsChatEvents;
 		this.supportsKey = haddon instanceof SupportsKeyEvents;
 		this.supportsIncomingMessages = haddon instanceof SupportsIncomingMessages;
@@ -95,10 +80,8 @@ public class HaddonBridgeModLoader extends BaseMod implements Manager, Bridge
 		this.supportsConnect = haddon instanceof SupportsConnectEvents;
 		
 		this.impl_continueTicking = this.supportsTick || this.supportsFrame;
-		this.impl_continueGuiTicking = this.supportsGuiTick || this.supportsGuiFrame;
 		
 		this.nextTick = -1;
-		this.lastGuiTick = -1;
 		
 		this.enlistedOutgoingMessages = new HashSet<String>();
 		if (this.supportsIncomingMessages)
@@ -126,86 +109,24 @@ public class HaddonBridgeModLoader extends BaseMod implements Manager, Bridge
 	{
 		this.haddon.onLoad();
 		
-		if (this.supportsTick && !this.supportsFrame)
-		{
-			ModLoader.setInGameHook(this, true, true);
-		}
-		else if (this.supportsFrame)
-		{
-			ModLoader.setInGameHook(this, true, false);
-		}
-		
-		if (this.supportsGuiTick && !this.supportsGuiFrame)
-		{
-			ModLoader.setInGUIHook(this, true, true);
-		}
-		else if (this.supportsGuiFrame)
-		{
-			ModLoader.setInGUIHook(this, true, false);
-		}
-		
+	}
+
+	@Override
+	public String getName()
+	{
+		return this.haddon.getIdentity().getHaddonName();
 	}
 	
 	@Override
-	public boolean onTickInGame(float semi, Minecraft minecraft)
+	public String getVersion()
 	{
-		if (this.supportsTick && this.tickEnabled)
-		{
-			/*long tick = this.utility.getClientTick();
-			if (tick != this.lastTick)
-			{
-				((SupportsTickEvents) this.haddon).onTick();
-				this.lastTick = tick;
-				
-			}*/
-			
-			// Shitty inaccurate workaround because the normal method has evolved
-			// (1.3.1 version can jump tick by running it multiple times at once
-			// and doesn't have a tick accumulator)
-			long tick = System.currentTimeMillis();
-			if (tick > this.nextTick)
-			{
-				((SupportsTickEvents) this.haddon).onTick();
-				this.nextTick = tick + 50;
-				this.ticksRan++;
-				
-			}
-			
-		}
-		
-		if (this.supportsFrame && this.frameEnabled)
-		{
-			((SupportsFrameEvents) this.haddon).onFrame(semi);
-			
-		}
-		
-		return this.impl_continueTicking;
-		
+		return this.haddon.getIdentity().getHaddonHumanVersion();
 	}
 	
 	@Override
-	public boolean onTickInGUI(float semi, Minecraft minecraft, GuiScreen gui)
+	public void onInitCompleted(Minecraft minecraft, LiteLoader loader)
 	{
-		if (this.supportsGuiTick && this.guiTickEnabled)
-		{
-			long tick = this.utility.getClientTick();
-			if (tick != this.lastGuiTick)
-			{
-				((SupportsGuiTickEvents) this.haddon).onGuiTick(gui);
-				this.lastGuiTick = tick;
-				
-			}
-			
-		}
-		
-		if (this.supportsGuiFrame && this.guiFrameEnabled)
-		{
-			((SupportsGuiFrameEvents) this.haddon).onGuiFrame(gui, semi);
-			
-		}
-		
-		return this.impl_continueGuiTicking;
-		
+		this.haddon.onLoad();
 	}
 	
 	@Override
@@ -225,26 +146,6 @@ public class HaddonBridgeModLoader extends BaseMod implements Manager, Bridge
 			throw new UnsupportedInterfaceException();
 		
 		this.tickEnabled = enable;
-		
-	}
-	
-	@Override
-	public void hookGuiTickEvents(boolean enable)
-	{
-		if (!this.supportsGuiTick)
-			throw new UnsupportedInterfaceException();
-		
-		this.guiTickEnabled = enable;
-		
-	}
-	
-	@Override
-	public void hookGuiFrameEvents(boolean enable)
-	{
-		if (!this.supportsGuiFrame)
-			throw new UnsupportedInterfaceException();
-		
-		this.guiFrameEnabled = enable;
 		
 	}
 	
@@ -410,4 +311,135 @@ public class HaddonBridgeModLoader extends BaseMod implements Manager, Bridge
 		
 	}
 	
+	@Override
+	public boolean onTickInGUI(float semi, Minecraft minecraft, GuiScreen gui)
+	{
+		if (this.supportsGuiTick && this.guiTickEnabled)
+		{
+			long tick = this.utility.getClientTick();
+			if (tick != this.lastGuiTick)
+			{
+				((SupportsGuiTickEvents) this.haddon).onGuiTick(gui);
+				this.lastGuiTick = tick;
+				
+			}
+			
+		}
+		
+		if (this.supportsGuiFrame && this.guiFrameEnabled)
+		{
+			((SupportsGuiFrameEvents) this.haddon).onGuiFrame(gui, semi);
+			
+		}
+		
+		return this.impl_continueGuiTicking;
+		
+	}
+	
+	@Override
+	public boolean onTickInGame(float semi, Minecraft minecraft)
+	{
+		if (this.supportsTick && this.tickEnabled)
+		{
+			/*long tick = this.utility.getClientTick();
+			if (tick != this.lastTick)
+			{
+				((SupportsTickEvents) this.haddon).onTick();
+				this.lastTick = tick;
+				
+			}*/
+			
+			// Shitty inaccurate workaround because the normal method has evolved
+			// (1.3.1 version can jump tick by running it multiple times at once
+			// and doesn't have a tick accumulator)
+			long tick = System.currentTimeMillis();
+			if (tick > this.nextTick)
+			{
+				((SupportsTickEvents) this.haddon).onTick();
+				this.nextTick = tick + 50;
+				this.ticksRan++;
+				
+			}
+			
+		}
+		
+		if (this.supportsFrame && this.frameEnabled)
+		{
+			((SupportsFrameEvents) this.haddon).onFrame(semi);
+			
+		}
+		
+		return this.impl_continueTicking;
+		
+	}
+	
+	@Override
+	public void onTick(Minecraft minecraft, float partialTicks, boolean inGame, boolean clock)
+	{
+		if (!this.shouldTick)
+			return;
+		
+		if (!inGame)
+			return;
+		
+		if (this.enableTick && clock)
+		{
+			if (this.suTick)
+			{
+				((SupportsTickEvents) this.haddon).onTick();
+			}
+			this.tickCounter++;
+		}
+		
+		if (this.enableFrame)
+		{
+			if (this.suFrame)
+			{
+				((SupportsFrameEvents) this.haddon).onFrame(partialTicks);
+			}
+		}
+	}
+	
+	@Override
+	public void setTickEnabled(boolean enabled)
+	{
+		this.enableTick = enabled;
+	}
+	
+	@Override
+	public void setFrameEnabled(boolean enabled)
+	{
+		this.enableFrame = enabled;
+	}
+	
+	@Override
+	public int getTicks()
+	{
+		return this.tickCounter;
+	}
+
+	@Override
+	public void hookGuiTickEvents(boolean enable) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void hookGuiFrameEvents(boolean enable) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void init(File configPath) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void upgradeSettings(String version, File configPath,
+			File oldConfigPath) {
+		// TODO Auto-generated method stub
+		
+	}
 }
